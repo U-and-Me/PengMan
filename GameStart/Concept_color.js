de5c728941daa5b72fe245bdb872c45d24b44445
Kim Yu Na <yunasun0920@gmail.com>
2021-11-02 10:53:39 +0900

    color.js 추가

---
+++ GameStart/Concept_color.js
@@ -0,0 +1,9 @@
+"use strict"
+
+function readTextFile(file){
+    let Wfile = new XMLHttpRequest();
+    Wfile.open("GET", "file", false);
+    Wfile.onreadystatechange = function(){
+        
+    }
+}
