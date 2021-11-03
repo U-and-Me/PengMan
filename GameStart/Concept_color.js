b8188e6c14e6d1d0a0b193cce561cae5bc585cce
Kim Yu Na <yunasun0920@gmail.com>
2021-11-03 23:22:08 +0900

    파일 테스트

--- GameStart/Concept_color.js
+++ GameStart/Concept_color.js
@@ -1,17 +1,28 @@
 "use strict"
 
-function readTextFile(file){
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false); //e
-    rawFile.onreadystatechange = function(){
-        if(rawFile.readyState === 4){
-            if(rawFile.status === 200 || rawFile.status == 0){
-                var allText = rawFile.responseText;
-                console.log(allText);
-            }
-        }
-    }
-    rawFile.send(null); //e
+function openTextFile() {
+    var input = document.createElement("input");
+    input.type = "file";
+    input.accept = "text/plain"; // 확장자가 xxx, yyy 일때, ".xxx, .yyy"
+    input.onchange = function (event) {
+        processFile(event.target.files[0]);
+    };
+    input.click();
+}
+function processFile(file) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        output.innerText = reader.result;
+    };
+    reader.readAsText(file, "euc-kr");
+}
+/*
+function processFile(file) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        alert(reader.result);
+    };
+    reader.readAsText(file, "utf-8");
 }
 
-readTextFile("../wordList/color/Level1.txt");
+processFile("../wordList/color/Level1.txt")*/
